14198aa4e06cfe85121b7eddc0a2059b9dcc8acd
Maxim Esin <esinmy@gmail.com>
2020-09-28 22:45:48 +0300

    Up to 47 minutes. Model, Repo and Controller created

---
+++ Data/MockCommanderRepo.cs
@@ -0,0 +1,23 @@
+using System.Collections.Generic;
+using Commander.Models;
+
+namespace Commander.Data
+{
+    public class MockCommanderRepo : ICommanderRepo
+    {
+        public IEnumerable<Command> GetAllCommands()
+        {
+            var commands = new List<Command> {
+                new Command { Id = 0, HowTo = "Boil and egg", Line = "Boild water", Platform = "Kettle" },
+                new Command { Id = 1, HowTo = "Boil and egg1", Line = "Boild water1", Platform = "Kettle1" },
+                new Command { Id = 2, HowTo = "Boil and egg2", Line = "Boild water2", Platform = "Kettle2" }
+            };
+            return commands;
+        }
+
+        public Command GetCommandById(int id)
+        {
+            return new Command { Id = 0, HowTo = "Boil and egg", Line = "Boild water", Platform = "Kettle" };
+        }
+    }
+}
