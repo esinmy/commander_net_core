bdfb1b4fe38fbf08cbcec11e4123cadc9c6f33c0
Maxim Esin <esinmy@gmail.com>
2020-09-29 22:46:56 +0300

    Saving data to DB. Added new method in IRepo and implemented it in SqlRepo

--- Data/MockCommanderRepo.cs
+++ Data/MockCommanderRepo.cs
@@ -5,6 +5,11 @@ namespace Commander.Data
 {
     public class MockCommanderRepo : ICommanderRepo
     {
+        public void CreateCommand(Command cmd)
+        {
+            throw new System.NotImplementedException();
+        }
+
         public IEnumerable<Command> GetAllCommands()
         {
             var commands = new List<Command> {
@@ -18,6 +23,11 @@ namespace Commander.Data
         public Command GetCommandById(int id)
         {
             return new Command { Id = 0, HowTo = "Boil and egg", Line = "Boild water", Platform = "Kettle" };
+        }
+
+        public bool SaveChanges()
+        {
+            throw new System.NotImplementedException();
         }
     }
 }
